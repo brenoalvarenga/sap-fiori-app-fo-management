159c73da28c1a4a4094419c2c59f9e0de9bdc125
brenoalvarenga <91996636+brenoalvarenga@users.noreply.github.com>
2025-05-14 18:28:38 +0000

    add `Inicio.view.xml` and `Inicio.controller.js`

---
+++ webapp/controller/Inicio.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+  ],
+  (Controller) => {
+    "use strict";
+
+    return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
+      onPress() {
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter.navTo("detail");
+      },
+    });
+  }
+);
